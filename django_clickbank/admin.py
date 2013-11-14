9900b22bf10e3116095f31e310d3cd80b493d20c
Chris Modjeska <kurisui@gmail.com>
2013-11-14 16:07:05 -0500

    Added admin action "resend_signals"

--- django_clickbank/admin.py
+++ django_clickbank/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django_clickbank.models import Notification, Post
 from django_clickbank.forms import PostAdminForm
-
+from django.conf import settings
 
 class NotificationAdmin(admin.ModelAdmin):
 
@@ -12,6 +12,18 @@ class NotificationAdmin(admin.ModelAdmin):
 	list_filter = ['transaction_type', 'transaction_date', 'verification_passed']
 	readonly_fields = ['post_data']
 
+	def resend_signals(self, request, queryset):
+		"""
+		Resend signals for notifications. This should probably only be used to debug
+		your application.
+		"""
+		for notification in queryset:
+			notification.send_signals()
+
+	resend_signals.short_description = 'Resend django signals for selected notifications'
+
+	if settings.CLICKBANK_DEBUG:
+		actions = [resend_signals]
 
 class PostAdmin(admin.ModelAdmin):
 
